d3fcd96ed046b4e0390a4f3c726e0cac2593c2f7
YaroslavChinyakov <chinyakov777@gmail.com>
2025-05-18 23:55:46 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -3,21 +3,21 @@
 #define INCLUDE_TRAIN_H_
 
 class Train {
-private:
+ private:
      struct Car {
         bool light;
         Car* next;
         Car* prev;
-    };
+     };
 
     int countOp;
     Car* first;
 
-public:
-    Train();                   
-    void addCar(bool light); 
-    int getLength();    
-    int getOpCount();      
+ public:
+     Train();                   
+    void addCar(bool light);
+    int getLength();
+    int getOpCount();
 };
 
 #endif  // INCLUDE_TRAIN_H_
