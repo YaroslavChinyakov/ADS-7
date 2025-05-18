c9770d35f99ddebe92118061ae4af0cf0926a300
YaroslavChinyakov <chinyakov777@gmail.com>
2025-05-18 21:27:22 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,8 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 #include <stdexcept>
-#include "train.h"
-#include <stdexcept>
 
 Train::Train()
     : countOp(0),
@@ -10,7 +8,6 @@ Train::Train()
 }
 
 void Train::addCar(bool light) {
-    
     Car* node = new Car;
     node->light = light;
     if (first == nullptr) {
@@ -29,21 +26,33 @@ void Train::addCar(bool light) {
 
 int Train::getLength() {
     countOp = 0;
-    
-    for (int k = 1;; ++k) {
-        Car* p = first;
-        for (int i = 0; i < k; ++i) {
-            p = p->next;
-            ++countOp;
+    if (!first) return 0;
+
+    Car* p = first;
+    do {
+        if (p->light) {
+            p->light = false;
         }
-        if (p == first) {
-            return k;
-        }
+        p = p->next;
+        ++countOp;
+    } while (p != first);
+
+    first->light = true;
+
+    p = first->next;
+    ++countOp;
+    int length = 1;
+    while (!p->light) {
+        p = p->next;
+        ++countOp;
+        ++length;
     }
+
+    first->light = false;
+
+    return length;
 }
 
 int Train::getOpCount() {
     return countOp;
 }
-
-
